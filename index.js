8bbcf9eef185447687e497dd7d6c2e11c1284713
Saintsid <123sid!@>
2023-02-17 16:29:12 +0300

    9 урок

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-const CC = require('currency-converter-lt')
-let currencyConverter = new CC({from:"USD", to:"RUB", amount:1})
+const express = require('express')
 
-currencyConverter.convert().then((response) => {
-    console.log(`Result: ${response}`) //or do something else
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('This home page')
+})
+
+app.get('/about', (req, res) => {
+    res.send('Про нас')
+})
+
+app.get('/user/:username/:id', (req, res) => {
+    res.send(`User ID: ${req.params.id}. Username: ${req.params.username}`)
+})
+
+const PORT = 3000
+
+app.listen(PORT, () => {
+    console.log(`Server started: http://localhost:${PORT}`)
 })
